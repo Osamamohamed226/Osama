a91dd7e9b991e21d21530f64098a5e76aa267bac
Yousef <yousefhamedhassan47@gmail.com>
2025-08-22 22:33:09 +0300

    AuthPage Not Done Sorry Boss Please Dont Fire Me 🙏🙏🙇‍️�‍

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomePage from './Page/HomePage'
 import { Routes,Route } from 'react-router-dom'
+import AuthPage from './Page/AuthPage/AuthPage'
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>}/>
+      <Route path='/auth' element={<AuthPage/>}/>
     </Routes>
   )
 }
