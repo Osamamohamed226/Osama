621054f42d7c17940fb6da3e7a590d0cf73bcc29
Yousef <yousefhamedhassan47@gmail.com>
2025-08-23 07:34:15 +0300

    navbar

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,10 +1,12 @@
 import NavBar from "../Components/Navbar/NavBar";
+import NavBar2 from "../Components/Navbar/NavBar2";
 
 export default function HomePage()
 {
     return(
         <>
             <NavBar/>
+            <NavBar2/>
             <div className="content"></div>
         </>
     )
