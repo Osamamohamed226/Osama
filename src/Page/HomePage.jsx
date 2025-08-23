a1dd33dff1dcfcc4c37ba8370d3f8d00a7a29d1b
Dunkmaster 🏀 <osamamohamed05650@gmail.com>
2025-08-23 07:35:02 +0300

    7amed el gamed

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,11 +1,31 @@
 import NavBar from "../Components/Navbar/NavBar";
+import '../Page/HomePage.css'
+import { Link } from 'react-router-dom'
 
-export default function HomePage()
-{
-    return(
-        <>
-            <NavBar/>
-            <div className="content"></div>
-        </>
-    )
+export default function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <div className="content">
+        <div className="header">
+          <div className="image"></div>
+          <div className="head-content">
+            <h1>Discover the Timeless Wonders of Egypt</h1>
+            <p>
+              Explore ancient temples, cruise the majestic Nile, and immerse
+              yourself in thousands of years of history and culture.
+            </p>
+            <div className="buttons">
+              <Link to="#" className="btn1">
+                Plan Your Adventure
+              </Link>
+              <Link to="#" className="btn2">
+                Explore Destinations
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
