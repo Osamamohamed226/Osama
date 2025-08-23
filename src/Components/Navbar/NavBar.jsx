8ba3c62738d9db7c6cb651989c292589d29dd495
Dunkmaster 🏀 <osamamohamed05650@gmail.com>
2025-08-23 06:38:29 +0300

    lastest

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,47 +1,55 @@
-import React from 'react'
-import './assets/css/NavBar.css'
-import { useState } from 'react';
+import React from "react";
+import "./assets/css/NavBar.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const languages = [
-  'English',
-  'العربية',
-  'Français',
-  'Deutsch',
-  'Español',
-  '中文',
+  "English",
+  "العربية",
+  "Français",
+  "Deutsch",
+  "Español",
+  "中文",
 ];
-
-
 
 function NavBar() {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <div className='navbar'>
-          <h1 className='logo'>EgyptGuide</h1>
-          <p>Destinations</p>
-          <p>Experiences</p>
-          <p>Plan Your Trip</p>
-          <p>About Egypt</p>
+      <div className="navbarr">
+        <div className="logo">
+          <h1>
+            <i class="text-orange fa-solid fa-ankh"></i>EgyptGuide
+          </h1>
+        </div>
+        <div className="right">
+          <div className="links">
+            <Link to="#">Destinations</Link>
+            <Link to="#">Experiences</Link>
+            <Link to="#">Plan Your Trip</Link>
+            <Link to="#">About Egypt</Link>
+          </div>
           <div className="language">
-      <button className="dropdown-toggle" onClick={() => setOpen(!open)}>
-        🌐 English <span className="arrow">{open ? '▲' : '▼'}</span>
-      </button>
-      {open && (
-        <ul className="dropdown-menu">
-          {languages.map((lang, index) => (
-            <li key={index} className="dropdown-item">
-              {lang}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-          <button className='itinerary'>My Itinerary</button>
+            <button className="dropdown-togglee" onClick={() => setOpen(!open)}>
+              🌐 English <span className="arrow">{open ? "▲" : "▼"}</span>
+            </button>
+            {open && (
+              <ul className="dropdown-menuu">
+                {languages.map((lang, index) => (
+                  <li key={index} className="dropdown-item">
+                    {lang}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+          <a Link="#">
+            <button>My Itinerary</button>
+          </a>
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
